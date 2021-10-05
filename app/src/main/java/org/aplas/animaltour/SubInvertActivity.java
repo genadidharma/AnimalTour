583c7025dd6496dc07ae3c6a4181033c3cf31ed3
Genadi Dharma <genadislawa@gmail.com>
2021-10-05 16:31:36 +0800

    B4 Task 8 - Write the code for InvertActivity which contains GridLayout for Invertebrates animal picture. Student also will write the code for SubInvertActivity.

--- app/src/main/java/org/aplas/animaltour/SubInvertActivity.java
+++ app/src/main/java/org/aplas/animaltour/SubInvertActivity.java
@@ -1,8 +1,12 @@
 package org.aplas.animaltour;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SubInvertActivity extends AppCompatActivity {
 
@@ -10,5 +14,36 @@ public class SubInvertActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_invert);
+        ImageView img = findViewById(R.id.mainPic);
+        TextView name = findViewById(R.id.mainName);
+        TextView detail = findViewById(R.id.mainDetail);
+        LinearLayout subLayout = findViewById(R.id.subLayout);
+        String title = getIntent().getStringExtra("TITLE_ANIMAL");
+        if (title != null) {
+            if (title.equals("Insects")) {
+                img.setImageResource(R.drawable.insects);
+                name.setText(title);
+                detail.setText(R.string.insect_content);
+            } else if (title.equals("Arachnids")) {
+                img.setImageResource(R.drawable.arachnids);
+                name.setText(title);
+                detail.setText(R.string.arachnid_content);
+            } else if (title.equals("Molluscs")) {
+                img.setImageResource(R.drawable.molluscs);
+                name.setText(title);
+                detail.setText(R.string.mollusc_content);
+            } else if (title.equals("Crustaceans")) {
+                img.setImageResource(R.drawable.crustaceans);
+                name.setText(title);
+                detail.setText(R.string.crustacean_content);
+            }
+        }
+        subLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAfterTransition();
+            }
+        });
+
     }
 }
