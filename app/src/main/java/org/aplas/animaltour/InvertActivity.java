0c36c61debb5a08b7fd7054cff6dd25fadbc6989
Genadi Dharma <genadislawa@gmail.com>
2021-10-05 14:14:40 +0800

    B4 Task 4 - Start to design the Invert layout as third layout that contains a GridLayout to show grid pictures.

---
+++ app/src/main/java/org/aplas/animaltour/InvertActivity.java
@@ -0,0 +1,14 @@
+package org.aplas.animaltour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InvertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invert);
+    }
+}
